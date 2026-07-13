fbc7f6c5b5a381feca06ea098ca0fff6c8d36b74
Ken-ichi Yokoi <boningori@gmail.com>
2026-07-13 09:39:35 +0900

    feat: 3Pシュート設定をスライド式トグルスイッチに変更

--- src/components/GameSetup/GameSetup.tsx
+++ src/components/GameSetup/GameSetup.tsx
@@ -473,28 +473,17 @@ export function GameSetup({ onComplete, onBack }: GameSetupProps) {
                                 </div>
                             </div>
 
-                            <div className="confirm-number-type">
+                            <div className="confirm-toggle-row">
                                 <span className="number-type-label">3Pシュート</span>
-                                <div className="number-type-options">
-                                    <label className={`number-type-option ${!showThreePoint ? 'selected' : ''}`}>
-                                        <input
-                                            type="radio"
-                                            name="showThreePoint"
-                                            checked={!showThreePoint}
-                                            onChange={() => setShowThreePoint(false)}
-                                        />
-                                        <span>使わない</span>
-                                    </label>
-                                    <label className={`number-type-option ${showThreePoint ? 'selected' : ''}`}>
-                                        <input
-                                            type="radio"
-                                            name="showThreePoint"
-                                            checked={showThreePoint}
-                                            onChange={() => setShowThreePoint(true)}
-                                        />
-                                        <span>使う</span>
-                                    </label>
-                                </div>
+                                <label className={`gs-toggle-switch ${showThreePoint ? 'on' : 'off'}`}>
+                                    <span className="gs-toggle-label">{showThreePoint ? '使う' : '使わない'}</span>
+                                    <input
+                                        type="checkbox"
+                                        checked={showThreePoint}
+                                        onChange={() => setShowThreePoint(!showThreePoint)}
+                                    />
+                                    <span className="gs-toggle-slider"></span>
+                                </label>
                             </div>
 
                             <div className="confirm-colors">
